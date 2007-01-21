70c4e4e6425b77c7c6834fad07799c4c7219973e
Nils Maier <maierman@web.de>
2007-01-21 03:13:28 +0000

    use standard way to provide default preferences.
    git-svn-id: https://code.downthemall.net/repos/trunk@35 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

---
+++ defaults/preferences/dta.js
@@ -0,0 +1,60 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is the downTHEMall preferences.
+ *
+ * The Initial Developer of the Original Code is Nils Maier
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Nils Maier <MaierMan@web.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+pref("extensions.dta.menu", "1,1,0");
+pref("extensions.dta.compactmenu", false);
+pref("extensions.dta.tool.menu", "1,1,1");
+pref("extensions.dta.tool.compactmenu", true);
+pref("extensions.dta.highlight", true);
+pref("extensions.dta.closetab", false);
+pref("extensions.dta.closedta", false);
+pref("extensions.dta.reduce", true);
+pref("extensions.dta.saveTemp", true);
+pref("extensions.dta.downloadWin", true);
+pref("extensions.dta.existing", 3);
+pref("extensions.dta.ntask",4);
+pref("extensions.dta.timeoutsel",6);
+pref("extensions.dta.maxchunks", 5);
+pref("extensions.dta.history", 5);
+pref("extensions.dta.alertbox", 2);
+pref("extensions.dta.removecompleted", true);
+pref("extensions.dta.removecanceled", false);
+pref("extensions.dta.removeaborted", false);
+pref("extensions.dta.infophrases", true);
+pref("extensions.dta.directory.visibledump", false);
+pref("extensions.dta.showOnlyFilenames", true);
+pref("extensions.dta.sounds.done", true);
+pref("extensions.dta.sounds.error", false);
+pref("extensions.dta.prealloc", true);
