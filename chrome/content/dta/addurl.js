298a4233d16b089a632841a9c688e6aae668e481
Nils Maier <maierman@web.de>
2007-03-30 20:26:20 +0000

    git-svn-id: https://code.downthemall.net/repos/branches/0.9.x@194 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

---
+++ chrome/content/dta/addurl.js
@@ -0,0 +1,287 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: GPL 2.0
+ *
+ * This code is part of DownThemAll! - dTa!
+ * Copyright © 2004-2006 Federico Parodi and Stefano Verna.
+ * 
+ * See notice.txt and gpl.txt for details.
+ *
+ * Contributers:
+ *   Nils Maier <MaierMan@web.de>
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var dropDowns = new Object();
+var strbundleB, strbundle;
+
+function checkSyntax() {
+	
+	// let's check and create the directory
+	var f = new filePicker();
+	var directory = $("directory");
+	if (!directory.value || directory.value.trim().length == 0) return false;
+	
+	if (f.createValidDestination(directory.value)==false) {
+		alert(strbundleB.getString("destination"));
+		var newDir = f.getFolder(null, strbundle.getString("validdestination"));
+		if (!newDir) 
+			directory.value="";
+		else
+			directory.value = newDir;
+		return false;
+	}
+	dropDowns.directory.saveCurrent(false);
+	return true;
+}
+
+function createEl(url, num) {
+	if (DTA_AddingFunctions.isLinkOpenable(url)) {
+		var el = {
+				'url': url,
+				'numIstance': num,
+				'refPage': $("URLref").value,
+				'description': window.arguments ? window.arguments[0].description : '',
+				'ultDescription': '',
+				'mask': dropDowns.renaming.getCurrent(),
+				'dirSave': dropDowns.directory.getCurrent()
+			};
+			return el;
+	} else
+		return null;
+}
+
+function addURLnow(start) {
+try {
+
+		if (!checkSyntax()) {
+			alert(strbundleB.getString("correctness"));
+			return false;
+		}
+		
+		var url = $("URLaddress").value;
+		
+		var num = Preferences.get("extensions.dta.numistance", 1);
+		num = (num<999)?(++num):1;
+		Preferences.set("extensions.dta.numistance", num);
+		
+		var address = $("URLaddress");
+		var url = address.value;
+		if (address.hasAttribute('_realURL'))	{
+			var url = { url: address.getAttribute('_realURL'), usable: address.value };
+		}
+			
+		var urlsToSend = [];
+		
+		var batch = new BatchGenerator(url);
+		if (!batch.isAString) {
+			var URL;
+			while ((URL=batch.getNextURL())!=null) {
+				var el = createEl(URL, num);
+				if (el) urlsToSend.push(el)
+			}
+		} else {
+			var el = createEl(url, num);
+			if (el) urlsToSend.push(el)
+		}
+		
+		if (urlsToSend.length == 0)  {
+			alert(strbundleB.getString("correctness"));
+			return false;
+		} else if (urlsToSend.length > 1) {
+			
+			var desc = strbundleB.getString("from") + "\n" + urlsToSend[0].url.cropCenter(70) + "\n";
+			if (urlsToSend.length > 2) desc += strbundleB.getString("to")+"\n";
+			desc += urlsToSend[urlsToSend.length-1].url.cropCenter(70);
+			
+			if (!confirm(strbundleB.getFormattedString("tasks",[urlsToSend.length, desc])))
+				return false;
+		}
+		
+		DTA_AddingFunctions.sendToDown(start, urlsToSend);
+		
+		dropDowns.directory.saveCurrent(false);
+		dropDowns.renaming.saveCurrent(false);
+		dropDowns.renaming.saveDrop(dropDowns.renaming.getCurrent());
+		dropDowns.directory.saveDrop(dropDowns.directory.getCurrent());
+			
+		window.close();
+		return true;
+} catch(e) {DTA_debug.dump("addURLnow(): ", e);}
+}
+
+function load() {try {
+		strbundleB = $("strings");
+		strbundle = $("string");
+		
+		$("directory").addEventListener("blur", checkSyntax, true);
+		
+		dropDowns.renaming = new dropDownObject("renaming", "renaming", "renamingitems", "*name*.*ext*", "*name*.*ext*|@|*num*_*name*.*ext*|@|*url*/*name*.*ext*|@|*name* (*text*).*ext*|@|*name* (*hh*-*mm*).*ext*");
+		dropDowns.renaming.load();
+		dropDowns.directory = new dropDownObject("directory", "directory", "directoryitems", "", "");
+		dropDowns.directory.load();
+		
+		// if we know the file to download
+		if (window.arguments) {
+			var e = window.arguments[0];
+			
+			if (!e.url.url) {
+				$("URLaddress").value = e.url;
+			} else {
+				$("URLaddress").value = e.url.usable;
+				// this dialog was fired up with a predefined url.
+				// changing it would invalidate the usable part of it.
+				// so making it readonly is the only option.
+				// if a users want to edit it then he should copy/paste the url. :p
+				// XXX: reflect readonly state in css
+				$("URLaddress").setAttribute('_realURL', e.url.url);
+				$("URLaddress").readonly = true;
+			}
+			
+			var refPage = DTA_AddingFunctions.isLinkOpenable(e.refPage) ? e.refPage : null;
+			if (refPage) {
+				try	{
+						refPage = decodeURIComponent(refPage);
+				} catch (ex) {}
+				$("URLref").value	 = refPage;
+			}
+		} else {
+			// check if there's some URL in clipboard
+			var clip = Components.classes["@mozilla.org/widget/clipboard;1"].getService(Components.interfaces.nsIClipboard);
+			var trans = Components.classes["@mozilla.org/widget/transferable;1"].createInstance(Components.interfaces.nsITransferable);
+			trans.addDataFlavor("text/unicode");
+			clip.getData(trans, clip.kGlobalClipboard);
+			var str=new Object();
+			var strLength=new Object();
+			trans.getTransferData("text/unicode",str,strLength);
+			if (str) {
+				str=str.value.QueryInterface(Components.interfaces.nsISupportsString);
+				pastetext=str.data.substring(0,strLength.value / 2);
+				if (pastetext.length > 0 && (/^(http|ftp)/).test(pastetext)) {
+					$("URLaddress").value = pastetext;
+					$("URLaddress").select();
+				}
+			}
+		}
+		
+	} catch(e) {
+	DTA_debug.dump("load():", e);
+	}
+}
+
+function browseDire() {
+	// let's check and create the directory
+	var f = new filePicker();
+	var newDir = f.getFolder($("directory").value, strbundle.getString("validdestination"));
+	if (newDir) $("directory").value = newDir;
+	
+	var directory = $("directory");
+	checkSyntax();
+	dropDowns.directory.saveCurrent(false);
+}
+
+function appendTag(event) {
+	var text = $(dropDowns.renaming.idInput);
+	var s = text.inputField.selectionStart;
+	text.value = text.value.substring(0, s) + event.target.getAttribute("value") + text.value.substring(text.inputField.selectionEnd, text.value.length);
+	text.inputField.setSelectionRange(s + event.target.getAttribute("value").length, s + event.target.getAttribute("value").length);
+}
+
+var listObserver = {
+  onDragStart: function (evt,transferData,action){
+    var txt=evt.target.getAttribute("value");
+    transferData.data=new TransferData();
+    transferData.data.addDataForFlavour("text/unicode", txt);
+  }
+}
+
+function unload() {
+}
+
+window.addEventListener("load", function() {setTimeout("window.sizeToContent();",0);}, false);
+
+function BatchGenerator(s) {
+	this.string = s;
+	
+	var mN = (/\[\s*(\d+)\s*-\s*(\d+)((\s+step\s*:?\s*\d*)?)\s*\]/).exec(s);
+	var mL = (/\[\s*([a-z]{1})\s*-\s*([a-z]{1})((\s+step\s*:?\s*\d*)?)\s*\]/i).exec(s);
+	this.isNumerical = (mN && (!mL|| mL.index>mN.index));
+	this.isLiteral =   (mL && (!mN|| mN.index>mL.index));
+	this.isAString = (this.isNumerical==null && this.isLiteral==null);
+
+	if (this.isAString) return;
+	
+	if (this.isNumerical) {
+		this.start=parseInt(mN[1],10);
+		this.end=parseInt(mN[2],10);
+		this.padding = "";
+		for (var i=((this.end>this.start)?mN[1]:mN[2]).length; (i--)>0;) {
+			this.padding=this.padding.concat("0");
+		}
+		this.match = mN;
+	} else if (this.isLiteral) {
+		 if(/[a-z]{1}/.test(mL[1]))
+				mL[2]=mL[2].toLowerCase();
+			else
+				mL[2]=mL[2].toUpperCase();
+				
+			this.start=mL[1].charCodeAt(0);
+			this.end=mL[2].charCodeAt(0);
+			this.match = mL;
+	}
+	
+	var sM=this.match[3].match(/\s+step\s*:?\s*(\d*)/);
+	this.step = (sM?parseInt(sM[1],10):1) * (this.start<=this.end?1:-1);
+	this.cursor=this.start;
+
+	this.nextBatch = new BatchGenerator(this.match.input.substring(this.match.index+this.match[0].length));
+}
+
+BatchGenerator.prototype = {
+
+	reset: function() {
+		this.cursor = this.start;
+		if (this.nextBatch) this.nextBatch.reset();	
+	},
+	
+	getNextURL: function() {
+   	
+    if (this.isAString) return this.string;
+    
+    if(
+    	this.step==0
+      || 
+      (this.step>0 && this.cursor>this.end)
+      || 
+      (this.step<0 && this.cursor<this.end)
+    ) {
+      return null;
+    }
+    
+    var count;
+    
+    if(this.isLiteral) {
+      count=String.fromCharCode(this.cursor);
+    } else {
+      count=new String(this.cursor);
+      if(count.length<this.padding.length) {
+        count=this.padding.substring(count.length).concat(count);
+      }
+    }
+    
+    var n = this.nextBatch.getNextURL();
+    
+    if (n==null || this.nextBatch.isAString) {
+      this.cursor += this.step;
+      if (n==null) {
+        this.nextBatch.reset();
+        return this.getNextURL();
+      }
+    }
+    
+    return this.match.input.substring(0, this.match.index).concat(count).concat(n);
+   
+  }
+
+}
+
+	
---
+++ chrome/content/dta/info.js
@@ -0,0 +1,194 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: GPL 2.0
+ *
+ * This code is part of DownThemAll! - dTa!
+ * Copyright © 2004-2006 Federico Parodi and Stefano Verna.
+ * 
+ * See notice.txt and gpl.txt for details.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var strbundle;
+var dropDowns = new Object();
+
+var canvas, d;
+
+function draw() {
+	
+	var c = d.firstChunk;
+	canvas.clearRect(0,0,300,20);
+
+	var prog = canvas.createLinearGradient(0,0,0,16);
+	prog.addColorStop(0, 'rgba(96,165,1,255)');
+	prog.addColorStop(1, 'rgba(123,214,1,255)');
+
+	var compl = canvas.createLinearGradient(0,0,0,16);
+	compl.addColorStop(0, 'rgba(13,141,15,255)');
+	compl.addColorStop(1, 'rgba(0,199,56,255)');
+	
+	var join = "#A5FE2C";
+	
+	var cancel = canvas.createLinearGradient(0,0,0,16);
+	cancel.addColorStop(0, 'rgba(151,58,2,100)');
+	cancel.addColorStop(1, 'rgba(255,0,0,100)');
+	
+	var normal = canvas.createLinearGradient(0,0,0,16);
+	normal.addColorStop(0, 'rgba(255,255,255,50)');
+	normal.addColorStop(1, '#ECE9D8');
+	
+	canvas.fillStyle = normal;
+	canvas.fillRect(0,0,300,20);
+
+	if (d.isCompleted) {
+		canvas.fillStyle = compl;
+		canvas.fillRect(0,0,300,20);
+		canvas.fillStyle = join;
+		if (d.join == null)
+			canvas.fillRect(0,16,300,4);
+		else
+			canvas.fillRect(0,16,Math.round(d.join.offset/d.totalSize*300),4);
+	} else if (d.isCanceled) {
+		canvas.fillStyle = cancel;
+		canvas.fillRect(0,0,300,20);
+	} else if (d.isStarted) {
+		while (c != -1) {
+			canvas.fillStyle=prog;
+			canvas.fillRect(Math.round(d.chunks[c].start/d.totalSize*300),0,Math.round(d.chunks[c].chunkSize/d.totalSize*300),20);
+			c = d.chunks[c].next;
+		}
+		canvas.fillStyle = join;
+		if (d.join == null)
+			canvas.fillRect(0,16,Math.round(d.chunks[d.firstChunk].chunkSize/d.totalSize*300),4);
+		else
+			canvas.fillRect(0,16,Math.round(d.join.offset/d.totalSize*300),4);
+	}
+	
+	setTimeout("draw()", 100);
+}
+
+function load() {
+try {
+	strbundle = document.getElementById("strings");
+	canvas = document.getElementById("draw").getContext("2d");
+	
+	document.getElementById("directory").addEventListener("blur", checkSyntax, true);
+	
+	// load dropdownns
+	dropDowns.renaming = new dropDownObject("renaming", "renaming", "renamingitems", "*name*.*ext*", "*name*.*ext*|@|*num*_*name*.*ext*|@|*url*/*name*.*ext*|@|*name* (*text*).*ext*|@|*name* (*hh*-*mm*).*ext*");
+	dropDowns.renaming.load();
+	dropDowns.directory = new dropDownObject("directory", "directory", "directoryitems", "", "");
+	dropDowns.directory.load();
+	
+	// d is an Array of Downloads
+	d = window.arguments[0];
+
+	if (d.length == 1) {
+		document.getElementById("infoURL").value = d[0].url;
+		document.getElementById("sourcePage").value = d[0].refPage.spec;
+		document.getElementById(dropDowns.renaming.idInput).value = d[0].mask;
+		document.getElementById(dropDowns.directory.idInput).value = d[0].originalDirSave;
+		document.getElementById("image").setAttribute("src", "moz-icon://"+ d.fileName+"?size=32");
+		d = d[0];
+		draw();
+	} else {
+		document.getElementById("infoURL").value = document.getElementById("sourcePage").value = "---";
+		
+		for (var count=1; count<d.length; count++)
+			if (String(d[count-1].mask) != String(d[count].mask)) break;
+		document.getElementById(dropDowns.renaming.idInput).value = (count==d.length)?d[0].mask:"---";
+	
+		for (var count=1; count<d.length; count++) 			
+			if (String(d[count-1].originalDirSave) != String(d[count].originalDirSave))
+				break;
+		
+		document.getElementById(dropDowns.directory.idInput).value = (count==d.length)?d[0].originalDirSave:"---";
+		
+		var normal = canvas.createLinearGradient(0,0,0,16);
+		normal.addColorStop(0, 'rgba(255,255,255,50)');
+		normal.addColorStop(1, '#ECE9D8');
+		
+		canvas.fillStyle = normal;
+		canvas.fillRect(0,0,300,20);
+	}
+} catch(e) {
+	alert(e);
+}
+}
+
+function unload() {
+	if (!checkSyntax()) return false;
+	
+	var t = window.arguments[0];
+	var win = window.arguments[1];
+	
+	for (var i=0; i<t.length; i++) {
+		d = t[i];
+		if (!(d.isCompleted && !d.isPassed)) {
+			if (document.getElementById(dropDowns.directory.idInput).value.trim().length>0)
+				d.originalDirSave = document.getElementById(dropDowns.directory.idInput).value.trim().addFinalSlash();
+
+			if (document.getElementById(dropDowns.renaming.idInput).value.trim().length>0)
+				d.mask = document.getElementById(dropDowns.renaming.idInput).value.trim();
+			
+			d.destinationName = d.fileName;
+			d.destinationName = d.buildFromMask(false, d.mask);
+			d.dirSave = d.originalDirSave;
+			d.dirSave = d.buildFromMask(true, d.mask);
+			
+			d.setTreeCell("dir", d.originalDirSave);
+			d.setTreeCell("mask", d.mask);
+			
+			d.checkFilenameConflict();
+		}
+	}
+	return true;
+}
+
+function browseDire() {
+	// let's check and create the directory
+	var f = new filePicker();
+	var newDir = f.getFolder(document.getElementById("directory").value, strbundle.getString("validdestination"));
+	if (newDir) document.getElementById("directory").value = newDir;
+	
+	var directory = document.getElementById("directory");
+	checkSyntax();
+	dropDowns.directory.saveCurrent(false);
+}
+
+function checkSyntax() {
+	// let's check and create the directory
+	var f = new filePicker();
+	var directory = document.getElementById("directory");
+	
+	if (directory.value.trim().length == 0) return false;
+	
+	if (!f.createValidDestination(directory.value)) {
+		alert(strbundle.getString("alertfolder"));
+		var newDir = f.getFolder(null, strbundle.getString("validdestination"));
+		if (!newDir)
+			directory.value="";
+		else
+			directory.value = newDir;
+
+		return false;
+	}
+	dropDowns.directory.saveCurrent(false);
+	return true;
+}
+
+function appendTag(event) {
+	var text = document.getElementById(dropDowns.renaming.idInput);
+	var s = text.inputField.selectionStart;
+	text.value = text.value.substring(0, s) + event.target.getAttribute("value") + text.value.substring(text.inputField.selectionEnd, text.value.length);
+	text.inputField.setSelectionRange(s + event.target.getAttribute("value").length, s + event.target.getAttribute("value").length);
+}
+
+var listObserver = {
+  onDragStart: function (evt,transferData,action){
+    var txt=evt.target.getAttribute("value");
+    transferData.data=new TransferData();
+    transferData.data.addDataForFlavour("text/unicode", txt);
+  }
+}
+
+window.addEventListener("load", function() {setTimeout("window.sizeToContent();",0);}, false);
