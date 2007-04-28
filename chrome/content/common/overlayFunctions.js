0088bd488c7e060c3c4123abf9b5c10e5df32a59
Stefano Verna <stefano.verna@welaika.com>
2007-04-28 10:38:38 +0000

    --

--- chrome/content/common/overlayFunctions.js
+++ chrome/content/common/overlayFunctions.js
@@ -20,7 +20,7 @@
  * Contributor(s):
  *   Nils Maier <MaierMan@web.de>
  *   Federico Parodi
- *   Stefano Verna
+ *   Stefano Verna <stefano.verna@gmail.com>
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
