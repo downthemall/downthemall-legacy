e32f8ab4cde0b93fc03462570eae74e0f29fd4c5
Nils Maier <maierman@web.de>
2007-06-07 22:23:28 +0000

    merging with source:branches/maierman/manager-trees patch available at http://code.downthemall.net/maierman/patches/dta-mt-trunk.diff
     * new download algorithm (one-file, recovering, failover)
     * new manager window architecture
     * verfication added (checksums)
     * metalink: added verficiation + file selection
     * link fingerprints added
    
    git-svn-id: https://code.downthemall.net/repos/trunk@395 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

---
+++ chrome/content/dta/manager/tooltip.js
@@ -0,0 +1,362 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is DownThemAll!
+ *
+ * The Initial Developers of the Original Code are Stefano Verna and Federico Parodi
+ * Portions created by the Initial Developers are Copyright (C) 2004-2007
+ * the Initial Developers. All Rights Reserved.
+ *
+ * Contributor(s):
+ *    Stefano Verna <stefano.verna@gmail.com>
+ *    Federico Parodi
+ *    Nils Maier <MaierMan@web.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+ 
+const TOOLTIP_FREQ = 500;
+var Tooltip = {
+	_current: null,
+	show: function(event) {
+		try {
+			if (!Preferences.getDTA("showtooltip", true)) {
+				return false;
+			}
+			var result;
+			var row = {};
+			Tree.box.getCellAt(event.clientX, event.clientY, row, {}, {});
+			if (row.value == -1) {
+				return false;
+			}
+			var d = Tree.at(row.value);
+			$("infoIcon").src = d.largeIcon;
+			$("infoURL").value = d.urlManager.url;
+			$("infoDest").value = d.destinationFile;
+	
+			this._current = d;
+			this.updateSpeedCanvas();
+			this.updateChunkCanvas();
+			
+			return true;
+		}
+		catch(ex) {
+			Debug.dump("Tooltip.show():", ex);
+		}
+		return false;
+	},
+	_makeRoundedRectPath: function(ctx,x,y,width,height,radius) {
+		ctx.beginPath();
+		ctx.moveTo(x, y + radius);
+		ctx.lineTo(x, y + height - radius);
+		ctx.quadraticCurveTo(x, y + height, x + radius, y + height);
+		ctx.lineTo(x + width - radius, y + height);
+		ctx.quadraticCurveTo(x + width, y + height, x + width,y + height - radius);
+		ctx.lineTo(x + width, y + radius);
+		ctx.quadraticCurveTo(x + width, y, x + width - radius, y);
+		ctx.lineTo(x + radius, y);
+		ctx.quadraticCurveTo(x, y, x, y + radius);
+	},
+	_createVerticalGradient: function(ctx, height, c1, c2) {
+		var g = ctx.createLinearGradient(0, 0, 0, height);
+		g.addColorStop(0, c1);
+		g.addColorStop(1, c2);
+		return g;
+	},
+	_createInnerShadowGradient: function(ctx, w, c1, c2, c3, c4) {
+		var g = ctx.createLinearGradient(0, 0, 0, w);
+		g.addColorStop(0, c1);
+		g.addColorStop(3.0 / w, c2);
+		g.addColorStop(4.0 / w, c3);
+		g.addColorStop(1, c4);
+		return g;
+	},
+	updateSpeedCanvas: function() {
+		var file = this._current;
+		if (!file) {
+			return;
+		}
+		try {
+			// we need to take care about with/height
+			var canvas = $("speedCanvas");
+			var width = canvas.width = canvas.clientWidth;
+			var height = canvas.height = canvas.clientHeight;
+			var ctx = canvas.getContext("2d");
+			--width; --height;
+			
+			var boxFillStyle = this._createInnerShadowGradient(ctx, height, "#B1A45A", "#F1DF7A", "#FEEC84", "#FFFDC4");
+			var boxStrokeStyle = this._createInnerShadowGradient(ctx, 8, "#816A1D", "#E7BE34", "#F8CC38", "#D8B231");
+			var graphFillStyle = this._createVerticalGradient(ctx, height - 7, "#FF8B00", "#FFDF38");
+			
+			ctx.clearRect(0, 0, width, height);
+			ctx.save();
+			ctx.translate(.5, .5);
+			
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = boxStrokeStyle;
+			ctx.fillStyle = boxFillStyle;
+				
+			// draw container chunks back
+			ctx.fillStyle = boxFillStyle;
+			this._makeRoundedRectPath(ctx, 0, 0, width, height, 5);
+			ctx.fill();
+	
+			var step = Math.floor(width / (SPEED_COUNT - 1));
+	
+			if (file.speeds.length > 2) {
+				var maxH, minH;
+				maxH = minH = file.speeds[0];
+				for (var i = 1, e = file.speeds.length; i < e; ++i) {
+					maxH = Math.max(maxH, file.speeds[i]);
+					minH = Math.min(minH, file.speeds[i]);
+				}
+				// special case: all speeds are the same
+				if (minH == maxH) {
+					var s = file.speeds.map(function(speed) { return 12; });
+				}
+				else {
+					var r = (maxH - minH);
+					var s = file.speeds.map(function(speed) { return 3 + Math.round((height - 6) * (speed - minH) / r); });
+				}
+
+				ctx.save();
+				ctx.clip();
+				[
+					{ x:4, y:0, f:this._createVerticalGradient(ctx, height - 7, "#EADF91", "#F4EFB1") },
+					{ x:2, y:0, f:this._createVerticalGradient(ctx, height - 7, "#DFD58A", "#D3CB8B") },
+					{ x:1, y:0, f:this._createVerticalGradient(ctx, height - 7, "#D0BA70", "#DFCF6F") },
+					{ x:0, y:0, f:graphFillStyle, s:this._createVerticalGradient(ctx, height - 7, "#F98F00", "#FFBF37") }
+				].forEach(
+					function(pass) {
+						ctx.fillStyle = pass.f;
+						var y = height + pass.y;
+						var x = pass.x + 0.5;
+								
+						ctx.beginPath();
+						ctx.moveTo(x, y);
+								
+						y = y - s[0];
+						ctx.lineTo(x, y);
+								
+						var slope = (s[1] - s[0]);
+						x = x + step * .7;
+						y = y - slope * .7;
+						ctx.lineTo(x, y);
+								
+						for (var j = 1, e = s.length - 1; j < e; ++j) {
+							x = x + step * .3;
+							y = y - slope *.3;
+	
+							slope = (s[j+1] - s[j]);
+							x = x + step * .3;
+							y = y - slope * .3;
+							ctx.quadraticCurveTo(step * j, height + pass.y - s[j], x, y);
+	
+							x = x + step * .4;
+							y = y - slope * .4;
+							ctx.lineTo(x, y);
+						}
+								
+						x = x + step * .3;
+						y = y - slope * .3;
+						ctx.lineTo(x, y);
+	
+						ctx.lineTo(x, height);
+						ctx.fill();
+								
+						if (pass.s) {
+							ctx.strokeStyle = pass.s;
+							ctx.stroke();
+						}
+					}
+				);
+				ctx.restore();
+			}
+			this._makeRoundedRectPath(ctx, 0, 0, width, height, 3);
+			ctx.stroke();
+				
+			ctx.restore();
+	
+			Dialog.setTimer('tooltip::speed', 'Tooltip.updateSpeedCanvas()', TOOLTIP_FREQ);
+		}
+		catch(ex) {
+			Debug.dump("updateSpeedCanvas(): ", ex);
+		}
+	},
+	updateChunkCanvas: function () {
+		var file = this._current;
+		if (!file) {
+			return;
+		}
+		
+		try {
+			$('infoSize').value = file.totalSize > 0 ? Utils.formatBytes(file.totalSize) : _('unknown');
+			if (file.is(RUNNING)) {
+				$('timeElapsed').value = Utils.formatTimeDelta((Utils.getTimestamp() - file.timeStart) / 1000);
+				$('timeRemaining').value = file.status;
+				$('speedCurrent').value = file.speed;
+				if (file.speeds.length) {
+					var avg = 0;
+					file.speeds.forEach(
+						function(s) {
+							avg += s;
+						}
+					)
+					$('speedAverage').value = Utils.formatBytes(avg / file.speeds.length) + "/s";
+				}
+				else {
+					$('speedAverage').value = _('unknown');
+				}
+			}
+			else {
+				$('timeElapsed', 'timeRemaining', 'speedCurrent', 'speedAverage').forEach(
+					function(e) {
+						e.value = _('nal');
+					}
+				);
+			}
+			var ip = $('infoPercent');
+			if (file.totalSize) {
+				ip.collapsed = false;
+				ip.value = file.percent;
+			}
+			else {
+				ip.collapsed = true;
+			}
+			var canvas = $("chunkCanvas");
+			var width = canvas.width = canvas.clientWidth;
+			var height = canvas.height = canvas.clientHeight;
+			var ctx = canvas.getContext("2d");
+			--width; --height;
+			
+			var cheight = height - 9;
+	
+			// Create gradients
+			var chunkFillStyle = this._createVerticalGradient(ctx, cheight, "#A7D533", "#D3F047");
+			var boxFillStyle = this._createInnerShadowGradient(ctx, cheight, "#B1A45A", "#F1DF7A", "#FEEC84", "#FFFDC4");
+			var boxStrokeStyle = this._createInnerShadowGradient(ctx, 8, "#816A1D", "#E7BE34", "#F8CC38", "#D8B231");
+			var partialBoxFillStyle = this._createInnerShadowGradient(ctx, 8, "#B1A45A", "#F1DF7A", "#FEEC84", "#FFFDC4");
+	
+			// clear all
+			ctx.clearRect(0, 0, width, height);
+			ctx.save();
+			ctx.translate(.5, .5);
+	
+			// draw container chunks back
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = boxStrokeStyle;
+			ctx.fillStyle = boxFillStyle;
+			this._makeRoundedRectPath(ctx, 0, 0, width, cheight, 5);
+			ctx.fill();
+	
+			var b = [];
+			if (file.is(COMPLETE)) {
+				b.push({
+					s: 0,
+					w: width
+				});
+			}
+			else if (!file.is(CANCELED)){
+				b = file.chunks.map(
+					function(chunk) {
+						if (file.totalSize <= 0) {
+							return {s:0, w: 0};
+						}
+						return {
+							s: Math.ceil(chunk.start / file.totalSize * width),
+							w: Math.ceil(chunk.written / file.totalSize * width)
+						};
+					}
+				);
+			}
+	
+			ctx.save();
+			ctx.clip();
+	
+			var passes = [
+				{ x:3, f: this._createInnerShadowGradient(ctx, cheight, "#AFA259", "#E8D675", "#F2E17E", "#F5F1B8") },
+				{ x:2, f: this._createInnerShadowGradient(ctx, cheight, "#9A8F4E", "#B0A359", "#B3A75D", "#BAB78B") },
+				{ x:1, f: this._createInnerShadowGradient(ctx, cheight, "#8E8746", "#B0A359", "#8E8746", "#CACB96") },
+				{ x:0, f: chunkFillStyle, s:chunkFillStyle }
+			];
+			
+		
+			b.forEach(
+				function(chunk) {
+					passes.forEach(
+						function(pass) {
+							ctx.fillStyle = pass.f;
+							this._makeRoundedRectPath(ctx, chunk.s + pass.x + 0.5, 0, chunk.w, cheight, 3);
+							ctx.fill();
+							if (pass.s) {
+								ctx.lineWidth = 2;
+								ctx.strokeStyle = pass.s;
+								ctx.stroke();
+							}
+						},
+						this
+					)
+				},
+				this
+			);
+			ctx.restore();
+	
+			// draw container chunks border
+			this._makeRoundedRectPath(ctx, 0, 0, width, cheight, 5);
+			ctx.stroke();
+	
+			// draw progress back
+			ctx.translate(0, cheight + 1);
+			ctx.fillStyle = partialBoxFillStyle;
+			this._makeRoundedRectPath(ctx, 0, 0, width, 8, 3);
+			ctx.fill();
+	
+			// draw progress
+			if (file.totalSize > 0) {
+				ctx.fillStyle = this._createVerticalGradient(ctx, 8, "#5BB136", "#A6D73E");
+				this._makeRoundedRectPath(ctx, 0, 0, Math.ceil(file.partialSize / file.totalSize * width), 8, 3);
+				ctx.fill();
+			}
+			else if (file.is(CANCELED)) {
+				ctx.fillStyle = this._createVerticalGradient(ctx, 8, "#B12801", "#FFFFFF");;
+				this._makeRoundedRectPath(ctx, 0, 0, width, 8, 3);
+				ctx.fill();
+			}
+	
+			// draw progress border
+			this._makeRoundedRectPath(ctx, 0, 0, width, 8, 3);
+			ctx.stroke();
+	
+			ctx.restore();
+	
+			Dialog.setTimer('tooltip:chunks', "Tooltip.updateChunkCanvas()", TOOLTIP_FREQ);
+	
+		} catch(ex) {
+			Debug.dump("updateChunkCanvas(): ", ex);
+		}
+	},
+	stopCanvas: function() {
+		this._current = null;
+	}
+}
---
+++ chrome/content/dta/manager/verificator.js
@@ -0,0 +1,130 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is DownThemAll!
+ *
+ * The Initial Developer of the Original Code is Nils Maier
+ * Portions created by the Initial Developer are Copyright (C) 2007
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *	 Nils Maier <MaierMan@web.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+ 
+ function Verificator(download) {
+	this.download = download;
+	this.file = new FileFactory(download.destinationFile);
+	this.cmp = download.hash.trim().toLowerCase();
+	this.CH = Ci.nsICryptoHash;
+
+	download.state = FINISHING;
+	download.status =  _("verify");
+	try {
+		this.type = DTA_checkHashFormat(this.cmp);
+		if (!(this.type in this.CH)) {
+			throw new Components.Exception("hash method unsupported!");
+		}
+		this.type = this.CH[this.type];
+		
+		this.hash = Cc["@mozilla.org/security/hash;1"]
+			.createInstance(this.CH);
+		this.hash.init(this.type);
+		
+		var nsIFI = Ci.nsIFileInputStream;
+		this.stream = Cc['@mozilla.org/network/file-input-stream;1']
+			.createInstance(Ci.nsIFileInputStream);
+		this.stream.init(this.file, 0x01, 0766, 0);
+		this.pump = Cc['@mozilla.org/network/input-stream-pump;1']
+			.createInstance(Ci.nsIInputStreamPump);
+		this.pump.init(this.stream, 0, -1, 0, 0, true);
+		this.pump.asyncRead(this, null);
+	}
+	catch (ex) {
+		try {
+			if (this.stream) {
+				this.stream.close();
+			}
+		}
+		catch (ex) {
+		}
+		alert("Failed to verify the file!\n" + ex);
+		download.complete();
+	}
+}
+Verificator.prototype = {
+	_delete: function() {
+		try {
+			if (this.file.exists()) {
+				this.file.remove(false);
+			}
+		}
+		catch (ex) {
+			alert("Failed to remove file\n" + ex);
+		}
+	},
+	_invalidate: function() {
+		this.download.invalidate();
+		var thisp = this;
+		Dialog.setTimer(this._uuid, function() { thisp._invalidate(); }, STREAMS_FREQ);
+	},
+	QueryInterface: function(iid) {
+		if (iid.equals(Ci.nsISupports) || iid.equals(Ci.nsIStreamListener) || iid.equals(cI.nsIRequestObserver)) {
+			return this;
+		}
+		throw Components.results.NS_ERROR_NO_INTERFACE;
+	},
+	onStartRequest: function(r, c) {
+		this._uuid = newUUIDString();
+		this._invalidate();
+	},
+	onStopRequest: function(request, c) {
+		Dialog.killTimer(this._uuid);
+		
+		this.download.partialSize = this.download.totalSize;
+		this.download.invalidate();
+		
+		this.hash = hexdigest(this.hash.finish(false));
+		if (this.hash != this.cmp) {
+			var act = DTA_confirm(_('verifyerrortitle'), _('verifyerrortext', [this.cmp, this.hash]), _('retry'), _('delete'), _('keep'));
+			switch (act) {
+				case 0: this._delete(); this.download.reDownload(); return;
+				case 1: this._delete(); this.download.cancel(); return;
+			}
+		}
+		this.download.complete();
+	},
+	onDataAvailable: function(request, c, stream, offset, count) {
+		try {
+			this.hash.updateFromStream(stream, count);
+			this.download.partialSize = offset;
+		}
+		catch (ex) {
+			Debug.dump("hash update failed!", ex);
+			var reason = 0x804b0002; // NS_BINDING_ABORTED;
+			request.cancel(reason);
+		}
+	}
+};
