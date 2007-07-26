c45b8f22574c95da402aff82f084191e2428012a
Nils Maier <maierman@web.de>
2007-07-26 23:06:24 +0000

    remember dta-s as default action. #51 Tried to hack this using MimeTypes, but that's simply an ugly mess. Actually IMO the whole saveAs dialog is an ugly mess :p

--- chrome/content/integration/saveas.js
+++ chrome/content/integration/saveas.js
@@ -1,10 +1,38 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: GPL 2.0
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * This code is part of DownThemAll! - dTa!
- * Copyright © 2004-2006 Federico Parodi and Stefano Verna.
- * 
- * See LICENSE and GPL for details.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is DownThemAll!
+ *
+ * The Initial Developers of the Original Code are Stefano Verna and Federico Parodi
+ * Portions created by the Initial Developers are Copyright (C) 2004-2007
+ * the Initial Developers. All Rights Reserved.
+ *
+ * Contributor(s):
+ *    Stefano Verna
+ *    Federico Parodi
+ *    Nils Maier <MaierMan@web.de>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
  *
  * ***** END LICENSE BLOCK ***** */
 
@@ -30,7 +58,6 @@ var DTA_SaveAs = {
 				},
 				0
 			);
-			
 		}
 		
 		if (!doOverlay) {
@@ -38,8 +65,15 @@ var DTA_SaveAs = {
 			// but we revert to help FlashGot ;)
 			return;
 		}
+		
+		this.normal = document.getElementById('downthemall');
+		this.turbo = document.getElementById('turbodta');
+		this.mode = document.getElementById('mode');
+		this.remember = document.getElementById("rememberChoice");
+		this.settingsChange = document.getElementById("settingsChange");
+		
 		document.getElementById('downthemallcontainer').collapsed = false;
-		document.getElementById('downthemall').disabled = false;
+		this.normal.disabled = false;
 		
 		this.dialog = dialog;
 		this.url = dialog.mLauncher.source.spec;
@@ -54,25 +88,42 @@ var DTA_SaveAs = {
 		this.ddDirectory = document.getElementById('tdtalist');
 		var mask = DTA_AddingFunctions.getDropDownValue('renaming');
 		if (!(document.getElementById("tdta").collapsed = (!DTA_AddingFunctions.getDropDownValue('directory') || !mask))) {
-			document.getElementById('turbodta').disabled = false;
+			this.turbo.disabled = false;
 		}
 		
-		
-		this.remember = document.getElementById("rememberChoice");
-		
-		// aggiungo la nostra proprietà senza sovrascrivere le rimanenti
-		document.documentElement.setAttribute('ondialogaccept', 'if(DTA_SaveAs.dialogAccepted()) { ' + document.documentElement.getAttribute('ondialogaccept') +'}');
-		
-		document.getElementById("mode").addEventListener("select", 
-			function(event) {
-				DTA_SaveAs.onSelect(); 
-			},  
+		try {
+			switch (DTA_preferences.getDTA('saveasmode', 0)) {
+				case 1:
+					this.mode.selectedItem = this.normal;
+					break;
+				case 2:
+					this.mode.selectedItem = this.turbo.disabled ? this.normal : this.turbo;
+					break;
+			}
+			if (DTA_preferences.getDTA('saveasmode', 0)) {
+				this.remember.checked = true;
+				this.remember.disabled = false;
+			}
+		}
+		catch (ex) {}
+
+		document.documentElement.setAttribute(
+			'ondialogaccept',
+			'if(DTA_SaveAs.dialogAccepted()) { '
+			+ document.documentElement.getAttribute('ondialogaccept')
+			+ '}'
+		);
+		this.mode.addEventListener(
+			'select',
+			function(evt) {
+				DTA_SaveAs.select(evt);
+			},
 			false
 		);
 	},
 	
 	revertUI: function dd_revertUI() {
-		['open', 'rememberChoice'].forEach(
+		['open'].forEach(
 			function(e) {
 				e = document.getElementById(e);
 				e.parentNode.collapsed = true;		
@@ -99,25 +150,15 @@ var DTA_SaveAs = {
 		}
 		catch (ex) {
 			window.sizeToContent();
-		}
+		}		
 	},
-
-	onSelect: function dd_select(event) {
-	/*
-		// problema di remember con Flashgot
-		var mode = document.getElementById("mode").selectedItem;
-		var dta = document.getElementById("downthemall");
-		var tdta = document.getElementById("turbodta");
-		var remember = document.getElementById("rememberChoice");
-		
-		if ( mode==dta || mode==tdta ) {
-			remember.setAttribute("disabled", "true");
-			alert("dta1");
-		} else {
-			remember.removeAttribute("disabled");
-			alert("dta2");
+	
+	select: function dd_select(evt) {
+		var mode = this.mode.selectedItem;
+		this.remember.checked = false;
+		if (this.normal == mode || this.turbo == mode) {
+			this.remember.disabled = false;
 		}
-	*/
 	},
 	
 	selectTurbo: function dd_selectTurbo(event) {
@@ -126,24 +167,21 @@ var DTA_SaveAs = {
 	},
 
 	dialogAccepted: function dd_accept() {
-		var mode = document.getElementById("mode").selectedItem;
-		var dta = document.getElementById("downthemall");
-		var tdta = document.getElementById("turbodta");
-		
-		DTA_preferences.setDTA("autoSaveDm.dta", mode==dta && this.remember.checked);
-		DTA_preferences.setDTA("autoSaveDm.tdta", mode==tdta && this.remember.checked);
-		
-		if (mode == dta) {
-			this.download(false);
-		  return false;
+		var mode = this.mode.selectedItem;
+		if (mode == this.normal || mode == this.turbo) {
+			if (this.remember.checked) {
+				DTA_preferences.setDTA("saveasmode", mode == this.normal ? 1 : 2);
+			}
+			else {
+				DTA_preferences.setDTA("saveasmode", 0);
+			}
+			this.download(mode == this.turbo);			
+			return false;
 		}
-		else if (mode == tdta) {
-			this.download(true);
-		  return false;
-		}
+		DTA_preferences.setDTA("saveasmode", 0);		
 	  return true;
 	},
-
+	
 	download: function(turbo) {
 		this.ddDirectory.save();
 		DTA_AddingFunctions.saveSingleLink(turbo, this.url, this.referrer, "");
