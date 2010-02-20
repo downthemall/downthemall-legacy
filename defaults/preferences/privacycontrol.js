9b6fcba0c01958eca44b892a358bc76fe00d1011
Nils Maier <maierman@web.de>
2010-02-20 18:24:37 +0000

    use correct set of defaults
    git-svn-id: https://code.downthemall.net/repos/trunk@1767 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- defaults/preferences/privacycontrol.js
+++ defaults/preferences/privacycontrol.js
@@ -1,2 +1,3 @@
 pref("extensions.{DDC359D1-844A-42a7-9AA1-88A850A938A8}.description", "chrome://dta/locale/dta.properties");
-pref("privacy.item.extensions-dta", false);
+pref("privacy.cpd.extensions-dta", false);
+pref("privacy.clearOnShutdown.extensions-dta", false);
