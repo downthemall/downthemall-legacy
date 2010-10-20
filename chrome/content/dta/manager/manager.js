4485340f84d7da079a68cced761f26477f6321bd
Nils Maier <maierman@web.de>
2010-10-20 16:31:56 +0000

    git-svn-id: https://code.downthemall.net/repos/branches/2.0.x@2160 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- chrome/content/dta/manager/manager.js
+++ chrome/content/dta/manager/manager.js
@@ -268,6 +268,8 @@ const Dialog = {
 		})();
 		
 		$('listSpeeds').limit = Prefs.speedLimit;
+		$('listSpeedsSpinners').addEventListener('up', function() Dialog.changeSpeedLimitUp(), false);
+		$('listSpeedsSpinners').addEventListener('down', function() Dialog.changeSpeedLimitDown(), false);
 		
 		(function nagging() {
 			if (Preferences.getExt('nagnever', false)) {
@@ -340,7 +342,14 @@ const Dialog = {
 		GlobalBucket.byteRate = val;
 		this._speeds.clear();
 	},
-	
+	changeSpeedLimitUp: function() {
+		$('listSpeeds').limit = Math.max(0, $('listSpeeds').limit) + 25600;
+		this.changeSpeedLimit();
+	},
+	changeSpeedLimitDown: function() {
+		$('listSpeeds').limit -= 25600;
+		this.changeSpeedLimit();
+	},
 	_loadDownloads: function D__loadDownloads() {
 		this._loading = $('loading');
 		if (!this._loading) {
