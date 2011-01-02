212e866e63942b37f31f5e276689726a28183d34
Nils Maier <maierman@web.de>
2011-01-02 02:38:09 +0000

    Make tray-on-close work with latest mintrayr
    git-svn-id: https://code.downthemall.net/repos/trunk@2363 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- chrome/content/dta/manager/manager.js
+++ chrome/content/dta/manager/manager.js
@@ -187,13 +187,7 @@ const Dialog = {
 		
 		(function initListeners() {
 			addEventListener('unload', function() Dialog.unload(), false);
-			addEventListener('close', function(evt) {
-				let rv = Dialog.close();
-				if (!rv) {
-					evt.preventDefault();
-				}
-				return rv;
-			}, true);
+			addEventListener('close', function(evt) Dialog.onclose(evt), false);
 			
 			window.DropProcessor = {
 				getSupportedFlavours: function() {
@@ -1042,6 +1036,13 @@ const Dialog = {
 	wasRemoved: function D_wasRemoved(d) {
 		this._running = this._running.filter(function(r) r != d);
 		this._autoRetrying = this._autoRetrying.filter(function(r) r != d);
+	},
+	onclose: function(evt) { 
+		let rv = Dialog.close();
+		if (!rv) {
+			evt.preventDefault();
+		}
+		return rv;
 	},
 	_canClose: function D__canClose() {
 		if (Tree.some(function(d) { return d.started && !d.resumable && d.isOf(RUNNING); })) {
