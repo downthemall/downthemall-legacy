2fbbfeb108a5cba8ddfebc5a19d032476f5a7f77
Nils Maier <maierman@web.de>
2011-07-02 00:29:19 +0000

    QUnit: constants.jsm

---
+++ tests/general/testConstants.js
@@ -0,0 +1,28 @@
+module("constants.jsm");
+
+test("exports", function() {
+	checkExports("resource://dta/constants.jsm", [
+		'PAUSED',
+		'RUNNING',
+		'FINISHING',
+		'COMPLETE',
+		'CANCELED',
+		'QUEUED',
+		'SPEED_COUNT',
+		'MIN_CHUNK_SIZE',
+		'REFRESH_FREQ',
+		'TOOLTIP_FREQ',
+	]);
+});
+
+test("powers", function() {
+	var _m = importModule("resource://dta/constants.jsm");
+	[
+		'PAUSED',
+		'RUNNING',
+		'FINISHING',
+		'COMPLETE',
+		'CANCELED',
+		'QUEUED',
+	].forEach(function(e,i) equal(_m[e] >> i, 2));
+});
