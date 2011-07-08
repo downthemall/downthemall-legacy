ccfb6ffe6f6b6582cf56b0f710c2c9154f643e2e
Nils Maier <maierman@web.de>
2011-07-08 03:23:27 +0000

    testPreallocator: use more sensible sizes

--- tests/manager/testPreallocator.js
+++ tests/manager/testPreallocator.js
@@ -15,7 +15,7 @@ module("preallocator.jsm");
 			var bytesPerSecond = ((size / 1048576.0) / (allocDiff / 1000.0)).toFixed(0);
 			ok(result, title);
 			if (result) {
-				equal(file.fileSize, size, "file size correct, run time " + (allocEnd - allocStart) + "ms, " + bytesPerSecond + " Mbytes/s");
+				equal(file.fileSize, size, "file size correct, run time " + (allocDiff) + "ms, " + bytesPerSecond + " Mbytes/s");
 			}
 			try {
 				file.remove(false);
@@ -30,29 +30,31 @@ module("preallocator.jsm");
 		checkExports("resource://dta/preallocation/cothread.jsm", ["prealloc_impl"]);
 	});
 
-	asyncTest("prealloc", function() {
-		var impl = importModule("resource://dta/manager/preallocator.jsm").prealloc;
-		_do_test("prealloc", impl, (1<<28), false);
-	});
-
 	try {
 		var impl = importModule("resource://dta/preallocation/worker.jsm").prealloc_impl;
-		asyncTest("worker non-sparse", function() _do_test("non-sparse", impl, (1<<28), false));
-		asyncTest("worker sparse", function() _do_test("non-sparse", impl, (1<<28), true));
+		asyncTest("prealloc", function() {
+			var impl = importModule("resource://dta/manager/preallocator.jsm").prealloc;
+			_do_test("prealloc", impl, (1<<30), false);
+		});
+		asyncTest("worker non-sparse", function() _do_test("non-sparse", impl, (1<<30), false));
+		asyncTest("worker sparse", function() _do_test("sparse", impl, (1<<30), true));
 	}
 	catch(ex) {
-		console.error(ex);
+		asyncTest("prealloc", function() {
+			var impl = importModule("resource://dta/manager/preallocator.jsm").prealloc;
+			_do_test("prealloc", impl, (1<<26), false);
+		});
 		test("worker", function() ok(true, "omitting worker: " + ex.message));
 	}
 
 	asyncTest("asynccopier", function() {
 		var impl = importModule("resource://dta/preallocation/asynccopier.jsm").prealloc_impl;
-		_do_test("asynccopier", impl, (1<<24));
+		_do_test("asynccopier", impl, (1<<26));
 	});
 
 	asyncTest("cothread", function() {
 		var impl = importModule("resource://dta/preallocation/cothread.jsm").prealloc_impl;
-		_do_test("cothread", impl, (1<<24));
+		_do_test("cothread", impl, (1<<26));
 	});
 
 })();
