5351e5787b1d90e50b2d7630f9998223cd371ca5
Nils Maier <maierman@web.de>
2012-05-12 23:14:30 +0000

    Port asyncmovefile to require

---
+++ modules/manager/asyncmovefile.js
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+const {
+	createOptimizedImplementation,
+	NullCancel
+} = require("support/optimpl");
+
+var _moveFile = createOptimizedImplementation(
+	"resource://dta/manager/asyncmovefile/worker.js",
+	function(impl) function _moveFile_async(aLocalFileSrc, aLocalFileDst, aPermissions, aCallback) {
+		let data = Object.create(null);
+		data.src = aLocalFileSrc.path;
+		data.dst = aLocalFileDst.path;
+		data.permissions = aPermissions;
+		return impl(data, aCallback);
+	},
+	function _moveFile_plain(aLocalFileSrc, aLocalFileDst, aPermissions, aCallback) {
+		try {
+			aLocalFileSrc.clone().moveTo(aLocalFileDst.parent, aLocalFileDst.leafName);
+			aCallback();
+		}
+		catch (ex) {
+			aCallback(ex);
+		}
+		return NullCancel;
+	});
+
+exports.asyncMoveFile = function asyncMoveFile(aLocalFileSrc, aLocalFileDst, aPermissions, aCallback) {
+	_moveFile.callImpl(aLocalFileSrc, aLocalFileDst, aPermissions, aCallback);
+}
