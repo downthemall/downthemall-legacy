00435f0dbea76cf741d18e93ea33009753b4a964
Nils Maier <maierman@web.de>
2012-05-12 23:16:13 +0000

    Port matcher to require

--- modules/manager/matcher.js
+++ modules/manager/matcher.js
@@ -1,57 +1,16 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is DownThemAll! Matcher module.
- *
- * The Initial Developer of the Original Code is Nils Maier
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *	 Nils Maier <MaierMan@web.de>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/ */
 "use strict";
 
-const EXPORTED_SYMBOLS = ['Matcher'];
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
-const module = Cu.import;
-const Exception = Components.Exception;
-
-let DTA = {};
-module('resource://dta/glue.jsm');
-module('resource://dta/api.jsm', DTA);
-requireJoined(DTA, "constants");
-module('resource://dta/utils.jsm');
-
-extendString(String);
+const {FilterManager} = require("support/filtermanager");
+const {COMPLETE, FINISHING} = require("constants");
+const {
+	Logger,
+	SimpleIterator,
+	StringBundles,
+	filterInSitu
+} = requireJSM("resource://dta/utils.jsm");
 
 (function(global) {
 	let strings = {};
@@ -68,7 +27,6 @@ extendString(String);
 	let bundles = new StringBundles(strings);
 	global['_'] = function() (arguments.length == 1) ? bundles.getString(arguments[0]) : bundles.getFormattedString.apply(bundles, arguments);
 })(this);
-
 
 const TextMatch = {
 	get name() 'textmatch',
@@ -91,7 +49,7 @@ const TextMatch = {
 const FilterMatch = {
 	get name() 'filtermatch',
 	getItems: function() {
-		for (let f in DTA.FilterManager.enumAll()) {
+		for (let f in FilterManager.enumAll()) {
 			if (f.id == "deffilter-all") {
 				continue;
 			}
@@ -105,7 +63,7 @@ const FilterMatch = {
 		let filters = [];
 		for each (let id in params) {
 			try {
-				filters.push(DTA.FilterManager.getFilter(id));
+				filters.push(FilterManager.getFilter(id));
 			}
 			catch (ex) {
 				if (Logger.enabled) {
@@ -120,7 +78,7 @@ const FilterMatch = {
 			}
 			return null;
 		}
-		let _m = DTA.FilterManager.getMatcherFor(filters);
+		let _m = FilterManager.getMatcherFor(filters);
 		return function(d) _m(d.urlManager.url.spec);
 	}
 };
@@ -200,8 +158,8 @@ const StatusMatch = {
 		},
 		getMatcher: function(params) {
 			let state = params.reduce(function(p,c) p | DTA[c], 0);
-			if (state & DTA.COMPLETE) {
-				state |= DTA.FINISHING;
+			if (state & COMPLETE) {
+				state |= FINISHING;
 			}
 			return function(d) d.state & state;
 		}
@@ -371,3 +329,5 @@ Matcher.prototype = {
 		return true;
 	}
 };
+
+exports.Matcher = Matcher;
