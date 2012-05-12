126ad766aafe8f3666c43939e717d064db2611f5
Nils Maier <maierman@web.de>
2012-05-12 23:16:42 +0000

    Port prealloctor to require

--- modules/manager/preallocator.js
+++ modules/manager/preallocator.js
@@ -1,27 +1,15 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/ */
-
 "use strict";
 
-const EXPORTED_SYMBOLS = [
-	'prealloc'
-];
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
-const Exception = Components.Exception;
-
-Cu.import("resource://dta/glue.jsm");
 const {
 	createOptimizedImplementation
 } = require("support/optimpl");
 const {Logger} = requireJSM("resource://dta/utils.jsm");
 
-const {prealloc: _asynccopier} = requireJSM("resource://dta/manager/preallocator/asynccopier.jsm");
-const {prealloc: _cothread} = requireJSM("resource://dta/manager/preallocator/cothread.jsm");
+const {prealloc: _asynccopier} = require("manager/preallocator/asynccopier");
+const {prealloc: _cothread} = require("manager/preallocator/cothread");
 
 const SIZE_MIN = (require("version").OS == 'winnt' ? 256 : 2048) * 1024;
 const SIZE_COTHREAD_MAX = (1<<24);
@@ -54,7 +42,7 @@ const _impl = createOptimizedImplementation(
  * @param tp (function) Scope (this) to call the callback function in
  * @return (nsICancelable) Pre-allocation object.
  */
-function prealloc(file, size, perms, sparseOk, callback) {
+exports.prealloc = function prealloc(file, size, perms, sparseOk, callback) {
 	if (size <= SIZE_MIN || !isFinite(size)) {
 		if (Logger.enabled) {
 			Logger.log("pa: not preallocating: " + file);
