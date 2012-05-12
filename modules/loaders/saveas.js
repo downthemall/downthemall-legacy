0df9aaf21aa7c4dcfc8e45f42aaf189c08f6e816
Nils Maier <maierman@web.de>
2012-05-12 23:17:10 +0000

    Port saveas to require
    git-svn-id: https://code.downthemall.net/repos/trunk@3128 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- modules/loaders/saveas.js
+++ modules/loaders/saveas.js
@@ -1,65 +1,18 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is DownThemAll API saveas loader.
- *
- * The Initial Developer of the Original Code is Nils Maier
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Nils Maier <MaierMan@web.de>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-const EXPORTED_SYMBOLS = ["load"];
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cu = Components.utils;
-const module = Cu.import;
-
-module("resource://dta/glue.jsm");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/ */
+"use strict";
 
 /* **
  * Lazy getters
  */
-XPCOMUtils.defineLazyGetter(this, 'DTA', function() {
-	let rv = {};
-	module("resource://dta/api.jsm", rv);
-	Object.freeze(rv);
-	return rv;
-});
-XPCOMUtils.defineLazyGetter(this, 'ContentHandling', function() require("support/contenthandling").ContentHandling);
+lazy(this, 'DTA', function() requireJSM("resource://dta/api.jsm"));
+lazy(this, 'ContentHandling', function() require("support/contenthandling").ContentHandling);
 
 /* **
  * Loader
  */
-function load(window) {
-	let document = window.document;
-	let dialog = window.dialog;
-
+exports.load = function load(window) {
 	function $() {
 		if (arguments.length == 1) {
 			return document.getElementById(arguments[0]);
@@ -77,7 +30,6 @@ function load(window) {
 		}
 		return elements;
 	}
-
 	function revertUI() {
 		['open'].forEach(
 			function(e) {
@@ -126,7 +78,6 @@ function load(window) {
 			}
 		}
 	}
-
 	function download(turbo) {
 		if (turbo) {
 			ddDirectory.save();
@@ -147,6 +98,9 @@ function load(window) {
 
 		de.cancelDialog();
 	}
+
+	let document = window.document;
+	let dialog = window.dialog;
 
 	let basicBox = $('basicBox');
 	let normalBox = $('normalBox');
