a5872a6712a637c03a432463d6718c540f058783
Nils Maier <maierman@web.de>
2012-05-12 23:14:03 +0000

    Port downloadHelper to require

--- modules/support/downloadHelper.js
+++ modules/support/downloadHelper.js
@@ -1,53 +1,19 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is DownThemAll DownloadHelper module.
- *
- * The Initial Developer of the Original Code is Nils Maier
- * Portions created by the Initial Developer are Copyright (C) 2009
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Nils Maier <MaierMan@web.de>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-const EXPORTED_SYMBOLS = ["available", "processors"];
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
-const module = Cu.import;
-const Exception = Components.Exception;
-
-module("resource://dta/glue.jsm");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const available = ("dhICore" in Ci) && ("dhIProcessor" in Ci);
-
 if (available) {
+	XPCOMUtils.defineLazyGetter(this, "api", function() {
+		let _m = {};
+		Cu.import("resource://dta/api.jsm", _m);
+		return _m;
+	});
+	XPCOMUtils.defineLazyGetter(this, "utils", function() {
+		let _m = {};
+		Cu.import("resource://dta/utils.jsm", _m);
+		return _m;
+	});
 	const core = Cc["@downloadhelper.net/core;1"].getService(Ci.dhICore);
 
 	function ProcessorImpl(turbo, name, title, description) {
@@ -73,8 +39,6 @@ if (available) {
 		preDownload: function(desc) false,
 
 		handle: function(props) {
-			module("resource://dta/utils.jsm");
-			module("resource://dta/api.jsm");
 			try {
 				if (props.has('links')) {
 					this.handleLinks(props);
@@ -84,8 +48,8 @@ if (available) {
 				}
 			}
 			catch (ex) {
-				if (Logger.enabled) {
-					Logger.log("failed to handle", ex);
+				if (utils.Logger.enabled) {
+					utils.Logger.log("failed to handle", ex);
 				}
 				throw ex;
 			}
@@ -94,10 +58,9 @@ if (available) {
 			return ('window' in props) ? props.window : null;
 		},
 		createItem: function(props) {
-			module("resource://dta/api.jsm");
 			let win = this.getWindow(props);
 			let doc = ('document' in props) ? props.document : null;
-			let url = new URL(Services.io.newURI(props.mediaUrl, doc ? doc.characterSet : null, null));
+			let url = new api.URL(Services.io.newURI(props.mediaUrl, doc ? doc.characterSet : null, null));
 			let item = {
 				url: url,
 				referrer: props.documentUrl || props.pageUrl || null,
@@ -121,8 +84,8 @@ if (available) {
 		handleLinks: function(desc) {
 			let links = desc.get('links', Ci.nsIArray).enumerate();
 			let urls = [];
-			for (let link in new SimpleIterator(links, Ci.nsIProperties)) {
-				let props = new Properties(link, desc);
+			for (let link in new utils.SimpleIterator(links, Ci.nsIProperties)) {
+				let props = new utils.Properties(link, desc);
 				let item = null;
 				try {
 					urls.push(this.createItem(props));
@@ -135,22 +98,22 @@ if (available) {
 				return;
 			}
 
-			let win = this.getWindow(new Properties(desc));
+			let win = this.getWindow(new utils.Properties(desc));
 			if (urls.length == 1) {
-				saveSingleItem(win, this.turbo, urls[0]);
+				api.saveSingleItem(win, this.turbo, urls[0]);
 				return;
 			}
 			if (this.turbo) {
-				turboSaveLinkArray(win, urls, []);
+				api.turboSaveLinkArray(win, urls, []);
 			}
 			else {
-				saveLinkArray(win, urls, []);
+				api.saveLinkArray(win, urls, []);
 			}
 		},
 		handleSingle: function(props)	{
-			props = new Properties(props);
+			props = new utils.Properties(props);
 			let item = this.createItem(props);
-			saveSingleItem(this.getWindow(props), this.turbo, item);
+			api.saveSingleItem(this.getWindow(props), this.turbo, item);
 		}
 	};
 
