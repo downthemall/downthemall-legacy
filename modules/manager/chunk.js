9b3ffdab29a7d00cf3352fbc32950cf6f8aa74e6
Nils Maier <maierman@web.de>
2012-05-12 23:16:52 +0000

    Port chunk to require

--- modules/manager/chunk.js
+++ modules/manager/chunk.js
@@ -1,54 +1,12 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is DownThemAll! Chunk module.
- *
- * The Initial Developer of the Original Code is Nils Maier
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *	 Nils Maier <MaierMan@web.de>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
 // XXX Consider using a real (single) pipe into the async stream copier
 // We don't do this for now, as the current scheme gives a little more control
 // about how data chunks are written to disk
 
-const EXPORTED_SYMBOLS = ["Chunk"];
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
-const module = Cu.import;
-
-module("resource://dta/glue.jsm");
 requireJoined(this, "constants");
 const Prefs = require("preferences");
 const {ByteBucketTee} = require("support/bytebucket");
@@ -56,8 +14,7 @@ const {GlobalBucket} = require("manager/globalbucket");
 const {defer} = require("support/defer");
 const {TimerManager} = require("support/timers");
 const Limits = require("support/serverlimits");
-
-module("resource://dta/utils.jsm");
+const {Logger, getTimestamp, formatNumber} = requireJSM("resource://dta/utils.jsm");
 
 const Timers = new TimerManager();
 
@@ -680,3 +637,5 @@ Chunk.prototype = {
 			};
 	}
 };
+
+exports.Chunk = Chunk;
