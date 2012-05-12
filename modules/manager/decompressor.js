65fab1b0c303d63ee8f1739eca23cd7a7ad3bb00
Nils Maier <maierman@web.de>
2012-05-12 23:17:35 +0000

    Port decompressor to require

--- modules/manager/decompressor.js
+++ modules/manager/decompressor.js
@@ -1,60 +1,15 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is DownThemAll! Decompressor module.
- *
- * The Initial Developer of the Original Code is Nils Maier
- * Portions created by the Initial Developer are Copyright (C) 2007
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *	 Nils Maier <MaierMan@web.de>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
-
-const EXPORTED_SYMBOLS = ['Decompressor'];
-
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/ */
+"use strict";
 
 const BUFFER_SIZE = 5 * 1024 * 1024;
 const FREQ = 250;
 
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
-const module = Cu.import;
-const Exception = Components.Exception;
-
-const DTA = {};
-module("resource://dta/glue.jsm");
+const DTA = requireJSM("resource://dta/api.jsm");
 const {TimerManager} = require("support/timers");
-
-module("resource://dta/utils.jsm");
-module("resource://dta/api.jsm", DTA);
-
-const Prefs = DTA.Preferences;
+const {Logger} = requireJSM("resource://dta/utils.jsm");
+const Prefs = require("preferences");
 
 const Timers = new TimerManager();
 
@@ -165,3 +120,5 @@ Decompressor.prototype = {
 		}
 	}
 };
+
+exports.Decompressor = Decompressor;
