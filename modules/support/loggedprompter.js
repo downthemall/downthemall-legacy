820427daaff612e6b35ec5987e250048546beb3b
Nils Maier <maierman@web.de>
2012-05-12 23:12:55 +0000

    Port loggedprompter to require
    git-svn-id: https://code.downthemall.net/repos/trunk@3092 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

---
+++ modules/support/loggedprompter.js
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+"use strict";
+
+XPCOMUtils.defineLazyGetter(this, "Logger", function() {
+	let _u = {};
+	module("resource://dta/utils.jsm", _u);
+	return _u.Logger;
+});
+
+/**
+ * Provides nsIPrompt/nsIAuthPrompt
+ * The nsIPrompt implementation will log any alerts instead of actually displaying them
+ *
+ * @param window Associated window (that will be the parent of any prompt dialogs)
+ */
+function LoggedPrompter(window) {
+	/**
+	 * Property providing nsIAuthPrompt
+	 */
+	lazy(this, "authPrompter", function() Services.ww.getNewAuthPrompter(window).QueryInterface(Ci.nsIAuthPrompt));
+
+	/**
+	 * Property providing nsIPrompt
+	 */
+	lazy(this,"prompter", function() {
+		let _p = Services.ww
+			.getNewPrompter(window)
+			.QueryInterface(Ci.nsIPrompt);
+
+		// Log any alerts instead of showing a dialog.
+		// Everything else pass thru to the actual prompter.
+		return Proxy.create({
+			has: function(name) name in _p,
+			hasOwn: function(name) name in _p,
+			get: function(receiver, name) {
+				if (name == "alert") {
+					return function(text, title) {
+						if (Logger.enabled) {
+							Logger.log(text, title);
+						}
+					}
+				}
+				else if (name == "alertCheck") {
+					return function(text, title) {
+						if (Logger.enabled) {
+							Logger.log(text, title);
+						}
+					}
+				}
+				else {
+					return _p[name];
+				}
+			}
+		});
+	});
+}
+exports.LoggedPrompter = LoggedPrompter;
