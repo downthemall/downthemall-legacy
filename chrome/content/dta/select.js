c413daac4cbf09bfce8005cd413448237e88a25f
Nils Maier <maierman@web.de>
2012-09-22 19:49:59 +0000

    Add use once option to renaming mask

--- chrome/content/dta/select.js
+++ chrome/content/dta/select.js
@@ -468,7 +468,9 @@ let Dialog = {
 
 			Preferences.setExt('seltab', this.current.type == 1 ? 0 : 1);
 			// save history
-			['ddDirectory', 'ddRenaming', 'ddFilter'].forEach(function (e) { Dialog[e].save(); });
+			this.ddRenaming.save($("renamingOnce").checked);
+			this.ddDirectory.save();
+			this.ddFilter.save();
 
 			// save the counter, queued state
 			Preferences.setExt("lastqueued", !start);
