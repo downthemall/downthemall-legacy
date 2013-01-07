2e2f36e871321a45e7f83108ba882d6cab6187c7
Nils Maier <maierman@web.de>
2013-01-07 19:22:43 +0000

    Add missing pbm test update
    git-svn-id: https://code.downthemall.net/repos/trunk@3382 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- tests/support/testPBM.js
+++ tests/support/testPBM.js
@@ -3,6 +3,7 @@ module("support/pbm.js");
 test("exports", function() {
 	checkExports("support/pbm", [
 		"isWindowPrivate",
+		"isChannelPrivate",
 		"registerPrivatePurger",
 		"unregisterPrivatePurger"
 		]);
