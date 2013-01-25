54ba77295455fe55fd6a1e77f19e09e9492159b3
Nils Maier <maierman@web.de>
2013-01-25 01:34:33 +0000

    Fix net-channel-event-sinks implementation (#2341)
    and more generally fix, category unregistration

--- modules/components.js
+++ modules/components.js
@@ -1,31 +1,43 @@
-exports.registerComponents = function registerComponents(components) {
+const {defer} = require("support/defer");
+
+exports.registerComponents = function registerComponents(components, direct) {
 	for (let cls of components) {
 		const factory = Object.freeze({
 			_cls: cls,
+			_info: (direct ? cls : cls.prototype),
+			_ctor: (direct ? (function(iid) this._cls.QueryInterface(iid)) : (function() new this._cls())),
 			createInstance: function(outer, iid) {
 				if (outer) {
 					throw Cr.NS_ERROR_NO_AGGREGATION;
 				}
-				return new this._cls();
+				return this._ctor(iid);
 			},
 			register: function() {
-				const cls = this._cls;
-				Cm.registerFactory(cls.prototype.classID, cls.prototype.classDescription, cls.prototype.contractID, this);
-				if (cls.prototype.xpcom_categories) {
-					for (let category of cls.prototype.xpcom_categories) {
-						Services.catman.addCategoryEntry(category, cls.prototype.classDescription, cls.prototype.contractID, false, true);
+				const i = this._info;
+				Cm.registerFactory(i.classID, i.classDescription, i.contractID, this);
+				if (i.xpcom_categories) {
+					for (let category of i.xpcom_categories) {
+						Services.catman.addCategoryEntry(category, i.classDescription, i.contractID, false, true);
 					}
 				}
 				unload(this.unregister.bind(this));
 			},
 			unregister: function() {
-				const cls = this._cls;
-				if (cls.prototype.xpcom_categories) {
-					for (let category of cls.prototype.xpcom_categories) {
-						Services.catman.deleteCategoryEntry(category, cls.prototype.classDescription, false);
+				const i = this._info;
+				if (i.xpcom_categories) {
+					for (let category of i.xpcom_categories) {
+						Services.catman.deleteCategoryEntry(category, i.classDescription, false);
 					}
 				}
-				Cm.unregisterFactory(this._cls.prototype.classID, this);
+				// defer, see bug 753687
+				defer((function(Cm, clsid) {
+					try {
+						Cm.unregisterFactory(clsid, this);
+					}
+					catch (ex) {
+						Components.utils.reportError(ex);
+					}
+				}).bind(this, Cm, i.classID));
 			}
 		});
 		factory.register();
