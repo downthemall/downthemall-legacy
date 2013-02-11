e89e96a665b462e5d0d20bfa3896b9812a22b370
Nils Maier <maierman@web.de>
2013-02-11 23:51:06 +0000

    Add testXML
    git-svn-id: https://code.downthemall.net/repos/trunk@3438 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

---
+++ tests/general/testXML.js
@@ -0,0 +1,47 @@
+module("XML");
+
+asyncTest("verify that UI XML actually loads", function() {
+	function runNext() {
+		var file = files.pop();
+		if (!file) {
+			QUnit.start();
+			return;
+		}
+		var req = XMLHttpRequest();
+		req.overrideMimeType("text/xml");
+		req.open("GET", "chrome://dta/content/" + file);
+		req.onloadend = function() {
+				runNext();
+				ok(req.responseXML && req.responseXML.documentElement.localName !== "parsererror", file);
+		};
+		req.send();
+	}
+	var files = [
+								"about/about.xul",
+								"common/bindings.xml",
+								"dta/addurl.xul",
+								"dta/manager-aero.xul",
+								"dta/manager.xul",
+								"dta/maskbutton.xml",
+								"dta/mirrors.xul",
+								"dta/select.xul",
+								"dta/manager/conflicts.xul",
+								"dta/manager/info.xul",
+								"dta/manager/manager.xul",
+								"dta/manager/metaselect.xul",
+								"integration/elements.xul",
+								"integration/saveas.xul",
+								"integration/toolbarinstall.xul",
+								"preferences/advPane.xul",
+								"preferences/bindings.xml",
+								"preferences/filtersPane.xul",
+								"preferences/interfacePane.xul",
+								"preferences/mainPane.xul",
+								"preferences/prefs.xul",
+								"preferences/privacyPane.xul",
+								"preferences/schedulePane.xul",
+								"preferences/serversPane.xul",
+								"privacy/overlaySanitize191.xul",
+								];
+	runNext();
+});
