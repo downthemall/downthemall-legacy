46c1fdfa330b3bb0aef6137d33c2c6403416d057
Nils Maier <maierman@web.de>
2013-06-12 23:03:03 +0000

    Add memoryPressure module
    git-svn-id: https://code.downthemall.net/repos/trunk@3570 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

---
+++ tests/support/testMemoryPressure.js
@@ -0,0 +1,29 @@
+module("support/memorypressure.js");
+
+test("exports", function() {
+	checkExports("support/memorypressure", ["add", "remove"]);
+});
+
+test("plain", 1, function() {
+	var mp = require("support/memorypressure");
+	var o = function(s, t, d) {
+		strictEqual(t, "memory-pressure");
+	}
+	mp.add(o);
+	Services.obs.notifyObservers(null, "memory-pressure", "heap-minimize");
+	mp.remove(o);
+	Services.obs.notifyObservers(null, "memory-pressure", "heap-minimize");
+});
+
+test("observer", 1, function() {
+	var mp = require("support/memorypressure");
+	var o = {
+		observe: function(s, t, d) {
+			strictEqual(t, "memory-pressure");
+		}
+	};
+	mp.add(o);
+	Services.obs.notifyObservers(null, "memory-pressure", "heap-minimize");
+	mp.remove(o);
+	Services.obs.notifyObservers(null, "memory-pressure", "heap-minimize");
+});
