aad1ab72a03ca442b93189092e4e42c69bf4d6cb
Nils Maier <maierman@web.de>
2014-04-05 12:37:44 +0000

    FileExtensionSheet: Do not produce rules for long extensions
    git-svn-id: https://code.downthemall.net/repos/trunk@3700 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- tests/support/testFileextsheet.js
+++ tests/support/testFileextsheet.js
@@ -8,8 +8,10 @@ test("getAtom", function() {
 	const {FileExtensionSheet} = require("support/fileextsheet");
 	var f = new FileExtensionSheet(window);
 	ok(f.getAtom("file.ext"));
-	strictEqual(f.getAtom("file.ext"), f.getAtom("file2.ext"));
-	strictEqual(f.getAtom("file.metalink", true), f.getAtom("file2.metalink", true));
-	strictEqual(f.getAtom("file.meta4", true), f.getAtom("file2.meta4", true));
-	strictEqual(f.getAtom("file.metalink", true), f.getAtom("file2.meta4", true));
+	strictEqual(f.getAtom("file.ext").toString(), f.getAtom("file2.ext").toString());
+	strictEqual(f.getAtom("file.metalink", true).toString(), f.getAtom("file2.metalink", true).toString());
+	strictEqual(f.getAtom("file.meta4", true).toString(), f.getAtom("file2.meta4", true).toString());
+	strictEqual(f.getAtom("file.metalink", true).toString(), f.getAtom("file2.meta4", true).toString());
+	strictEqual(f.getAtom("file.downthemall is dope").toString(), "FileIconunknown");
+	strictEqual(f.getAtom("file.downthemall is dope").toString(), f.getAtom("file.downthemall is doper").toString());
 });
