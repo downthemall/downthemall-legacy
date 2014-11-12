512ff8d902839d2fb3fed7c81a3a1967fe73006d
Nils Maier <maierman@web.de>
2014-11-12 03:55:15 +0000

    Fix save-as integration
    git-svn-id: https://code.downthemall.net/repos/trunk@3776 43940e1f-b481-bb45-89eb-5c6a4bf3d40a

--- modules/loaders/saveas.js
+++ modules/loaders/saveas.js
@@ -15,7 +15,7 @@ lazy(this, "isWindowPrivate", function() require("support/pbm").isWindowPrivate)
 /* **
  * Loader
  */
-exports.load = function load(window, document) {
+function load(window, document) {
 	function $() {
 		if (arguments.length == 1) {
 			return document.getElementById(arguments[0]);
@@ -33,175 +33,186 @@ exports.load = function load(window, document) {
 		}
 		return elements;
 	}
-	function revertUI() {
-		var nodes = normalBox.querySelectorAll("separator");
-		for (var i = 0; i < nodes.length; ++i) {
-			nodes[i].collapsed = true;
-		}
 
-		basicBox.collapsed = true;
-		normalBox.collapsed = false;
+	log(LOG_ERROR, "save-as int");
+	try {
+		const dialog = window.dialog;
 
-		// take care of FlashGot... for now.
-		// need to negotiate with the author (and possible other extension authors)
-		try {
-			gFlashGotDMDialog.init();
-			$("flashgot-basic").collapsed = true;
-		}
-		catch (ex) {
-			// no op
-		}
+		const basicBox = $('basicBox');
+		const normalBox = $('normalBox');
+		const normal = $('downthemall');
+		const turbo = $('turbodta');
+		const turboExec = $('turbodtaexec');
+		const mode = $('mode');
+		const remember = $("rememberChoice");
+		const settingsChange = $("settingsChange");
+		const ddDirectory = $('tdtalist');
 
-		// Workaround for bug 371508
-		try {
-			window.sizeToContent();
-		}
-		catch (ex) {
-			log(LOG_DEBUG, "sizeToContent Bug: 371508", ex);
+		const revertUI = () => {
+			let nodes = normalBox.querySelectorAll("separator");
+			for (let i = 0; i < nodes.length; ++i) {
+				nodes[i].collapsed = true;
+			}
+
+			basicBox.collapsed = true;
+			normalBox.collapsed = false;
+
+			// take care of FlashGot... for now.
+			// need to negotiate with the author (and possible other extension authors)
 			try {
-				var btn = document.documentElement.getButton('accept');
-				window.innerHeight = btn.boxObject.y + 10;
+				gFlashGotDMDialog.init();
+				$("flashgot-basic").collapsed = true;
 			}
 			catch (ex) {
-				log(LOG_ERROR, "setting height failed", ex);
+				// no op
 			}
+
+			// Workaround for bug 371508
+			try {
+				window.sizeToContent();
+			}
+			catch (ex) {
+				log(LOG_DEBUG, "sizeToContent Bug: 371508", ex);
+				try {
+					var btn = document.documentElement.getButton('accept');
+					window.innerHeight = btn.boxObject.y + 10;
+				}
+				catch (ex) {
+					log(LOG_ERROR, "setting height failed", ex);
+				}
+			}
+			log(LOG_ERROR, "reverted!");
 		}
-	}
-	var download = function download(turbo) {
-		if (turbo) {
-			ddDirectory.save();
-		}
-		let item = {
-			"url": url,
-			"referrer": referrer,
-			"description": "",
-			"isPrivate": isPrivate
+		let download = turbo => {
+			if (turbo) {
+				ddDirectory.save();
+			}
+			let item = {
+				"url": url,
+				"referrer": referrer,
+				"description": "",
+				"isPrivate": isPrivate
+			};
+
+			DTA.saveSingleItem(window, turbo, item);
+			let de = document.documentElement;
+			try {
+				de.removeAttribute('ondialogaccept');
+			}
+			catch (ex) {
+				// no op
+			}
+			try {
+				const NS_BINDING_ABORTED = 0x804b0002;
+				dialog.mLauncher.cancel(NS_BINDING_ABORTED);
+			}
+			catch (ex) {}
+
+			de.cancelDialog();
+
+			// avoid users double-clicking or something
+			download = function() {};
 		};
 
-		DTA.saveSingleItem(window, turbo, item);
-		let de = document.documentElement;
+		let url, referrer, mask, isPrivate;
+
+		// Need to get behind the default load event
+		const doOverlay = Preferences.getExt("downloadWin", true);
+
+		if (!doOverlay && typeof(gFlashGotDMDialog) == 'undefined') {
+			log(LOG_DEBUG, "not doing anything");
+			// we do not actually overlay!
+			return;
+		}
+		revertUI();
+
+		if (!doOverlay) {
+			// we do not actually overlay!
+			// but we revert to help FlashGot ;)
+			log(LOG_DEBUG, "not overlaying");
+			return;
+		}
+		$('downthemallcontainer').collapsed = false;
+		normal.disabled = false;
+
+		isPrivate = isWindowPrivate(dialog.mContext);
+
+		url = ContentHandling.getRedirect(dialog.mLauncher.source, isPrivate);
 		try {
-			de.removeAttribute('ondialogaccept');
+			referrer = dialog.mContext.QueryInterface(Ci.nsIWebNavigation).currentURI.spec;
+		}
+		catch(ex) {
+			referrer = url.spec;
+		}
+
+		let ml = DTA.getLinkPrintMetalink(url);
+		url = new DTA.URL(ml ? ml : url);
+
+		ddDirectory.isPrivate = isPrivate;
+		mask = DTA.getDropDownValue('renaming', isPrivate);
+		if (!($("tdta").hidden = (!DTA.getDropDownValue('directory', isPrivate) || !mask))) {
+			turbo.disabled = false;
+			turboExec.disabled = false;
+		}
+
+		try {
+			switch (Preferences.getExt('saveasmode', 0)) {
+				case 1:
+					mode.selectedItem = normal;
+					break;
+				case 2:
+					mode.selectedItem = turbo.disabled ? normal : turbo;
+					break;
+			}
+			if (Preferences.getExt('saveasmode', 0)) {
+				remember.checked = true;
+				remember.disabled = false;
+			}
 		}
 		catch (ex) {
 			// no op
 		}
-		try {
-			const NS_BINDING_ABORTED = 0x804b0002;
-			dialog.mLauncher.cancel(NS_BINDING_ABORTED);
-		}
-		catch (ex) {}
 
-		de.cancelDialog();
+		mode.addEventListener(
+			'select',
+			function() {
+				let selMode = mode.selectedItem;
+				remember.checked = false;
+				if (normal == selMode || turbo == selMode) {
+					remember.disabled = false;
+				}
+			},
+			false
+		);
+		ddDirectory.addEventListener('command', function() {
+			mode.selectedItem = turbo;
+		}, true);
 
-		// avoid users double-clicking or something
-		download = function() {};
-	};
+		turboExec.addEventListener('command', function() {
+			download(true);
+		}, true);
 
-	const dialog = window.dialog;
-
-	const basicBox = $('basicBox');
-	const normalBox = $('normalBox');
-	const normal = $('downthemall');
-	const turbo = $('turbodta');
-	const turboExec = $('turbodtaexec');
-	const mode = $('mode');
-	const remember = $("rememberChoice");
-	const settingsChange = $("settingsChange");
-	const ddDirectory = $('tdtalist');
-
-	let url, referrer, mask, isPrivate;
-
-	// Need to get behind the default load event
-	const doOverlay = Preferences.getExt("downloadWin", true);
-
-	if (!doOverlay && typeof(gFlashGotDMDialog) == 'undefined') {
-		log(LOG_DEBUG, "not doing anything");
-		// we do not actually overlay!
-		return;
-	}
-	revertUI();
-
-	if (!doOverlay) {
-		// we do not actually overlay!
-		// but we revert to help FlashGot ;)
-		log(LOG_DEBUG, "not overlaying");
-		return;
-	}
-	$('downthemallcontainer').collapsed = false;
-	normal.disabled = false;
-
-	isPrivate = isWindowPrivate(dialog.mContext);
-
-	url = ContentHandling.getRedirect(dialog.mLauncher.source, isPrivate);
-	try {
-		referrer = dialog.mContext.QueryInterface(Ci.nsIWebNavigation).currentURI.spec;
-	}
-	catch(ex) {
-		referrer = url.spec;
-	}
-
-	let ml = DTA.getLinkPrintMetalink(url);
-	url = new DTA.URL(ml ? ml : url);
-
-	ddDirectory.isPrivate = isPrivate;
-	mask = DTA.getDropDownValue('renaming', isPrivate);
-	if (!($("tdta").hidden = (!DTA.getDropDownValue('directory', isPrivate) || !mask))) {
-		turbo.disabled = false;
-		turboExec.disabled = false;
-	}
-
-	try {
-		switch (Preferences.getExt('saveasmode', 0)) {
-			case 1:
-				mode.selectedItem = normal;
-				break;
-			case 2:
-				mode.selectedItem = turbo.disabled ? normal : turbo;
-				break;
-		}
-		if (Preferences.getExt('saveasmode', 0)) {
-			remember.checked = true;
-			remember.disabled = false;
-		}
-	}
-	catch (ex) {
-		// no op
-	}
-
-	mode.addEventListener(
-		'select',
-		function() {
+		window.addEventListener('dialogaccept', function(evt) {
 			let selMode = mode.selectedItem;
-			remember.checked = false;
-			if (normal == selMode || turbo == selMode) {
-				remember.disabled = false;
+			if (selMode == normal || selMode == turbo) {
+				if (remember.checked) {
+					Preferences.setExt("saveasmode", selMode == normal ? 1 : 2);
+				}
+				else {
+					Preferences.setExt("saveasmode", 0);
+				}
+				download(selMode == turbo);
+				evt.stopPropagation();
+				evt.preventDefault();
+				return;
 			}
-		},
-		false
-	);
-	ddDirectory.addEventListener('command', function() {
-		mode.selectedItem = turbo;
-	}, true);
+			Preferences.setExt("saveasmode", 0);
+		}, false); // dialogaccept
+	} catch (ex) {
+		log(LOG_ERROR, "failed to run save-as int", ex);
+	}
+}
 
-	turboExec.addEventListener('command', function() {
-		download(true);
-	}, true);
-
-	window.addEventListener('dialogaccept', function(evt) {
-		let selMode = mode.selectedItem;
-		if (selMode == normal || selMode == turbo) {
-			if (remember.checked) {
-				Preferences.setExt("saveasmode", selMode == normal ? 1 : 2);
-			}
-			else {
-				Preferences.setExt("saveasmode", 0);
-			}
-			download(selMode == turbo);
-			evt.stopPropagation();
-			evt.preventDefault();
-			return;
-		}
-		Preferences.setExt("saveasmode", 0);
-	}, false); // dialogaccept
+exports.load = function(w, d) {
+	w.setTimeout(load.bind(null, w, d), 0);
 };
