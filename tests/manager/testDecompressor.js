f2f56c41ae7e5d4d258c15a0feaecd54989a379c
Nils Maier <maierman@web.de>
2016-03-15 11:46:07 +0000

    Tests: decompressor

---
+++ tests/manager/testDecompressor.js
@@ -0,0 +1,59 @@
+"use strict";
+module("manager/decompressor.js");
+
+test("exports", function() {
+	checkExports("manager/decompressor", ["Decompressor"]);
+});
+
+asyncTest("decompress something", function() {
+	const {OS} = requireJSM("resource://gre/modules/osfile.jsm");
+	const {Task} = requireJSM("resource://gre/modules/Task.jsm");
+	Task.spawn(function*() {
+		try {
+			let base = getRelURI("data/compressed.gz");
+			console.log(base.spec);
+			let file = FileUtils.getFile("TmpD", ["dta-test-compressed.gz"]);
+			let out = FileUtils.getFile("TmpD", ["dta-test-uncompressed"]);
+
+			function get() {
+				return new Promise((res, rej) => {
+					let content = new XMLHttpRequest();
+					content.open("GET", base.spec);
+					content.responseType = "arraybuffer";
+					content.onloadend = function(e) {
+						res(content);
+					};
+					content.send();
+				});
+			}
+
+			let content = yield get();
+			content = new Uint8Array(content.response);
+			yield OS.File.writeAtomic(file.path, content);
+
+			let download = {
+				destinationLocalFile: out,
+				tmpFile: file,
+				compression: "gzip",
+				invalidate: function() {
+					this.invcalled = true;
+				}
+			};
+			const {Decompressor} = require("manager/decompressor");
+			let res = yield new Promise((res, rej) => {
+				new Decompressor(download, res);
+			});
+			equal(!!res, false);
+			if (res) {
+				throw res;
+			}
+			equal(out.fileSize, 276);
+		}
+		catch (ex) {
+			console.error(ex);
+		}
+		finally {
+			QUnit.start();
+		}
+	});
+});
